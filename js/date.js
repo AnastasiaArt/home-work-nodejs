c5f5eef0f7334118a14aea3fd3a4a3a1bb6774c3
Artemyeva Anastasiya <art-nastya@inbox.ru>
2021-09-13 14:18:24 +0300

    add class for date

--- js/date.js
+++ js/date.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 const yargs = require('yargs/yargs')
 const {hideBin} = require('yargs/helpers')
-const moment = require('moment')
+const CurrentDate = require('./utils/current-date.js')
+const CalcDate = require('./utils/calc-date.js')
 
 yargs(hideBin(process.argv))
     .option('year', {
@@ -12,7 +13,7 @@ yargs(hideBin(process.argv))
         alias: 'm',
         desc: 'месяц',
     })
-    .option('days', {
+    .option('day', {
         alias: 'd',
         desc: 'день',
     })
@@ -20,58 +21,26 @@ yargs(hideBin(process.argv))
         command: 'current',
         desc: 'Текущая дата',
         handler: (argv) => {
-            const date = switchCurrentDate(argv)
-            console.log(date)
+            const date = new CurrentDate(argv)
+            console.log(date.parseDate())
         }
     })
     .command({
-            command: 'add',
-            desc: 'Увеличить -y[value](год), -m[value](месяц), -d[value](день)',
-            handler: (argv) => {
-                const date = switchCalcDate(argv, 'add')
-                console.log(date)
-            }
-        })
+        command: 'add',
+        desc: 'Увеличить -y[value](год), -m[value](месяц), -d[value](день)',
+        handler: (argv) => {
+            const date = new CalcDate(argv, 'add')
+            console.log(date.parseDate())
+        }
+    })
     .command({
         command: 'sub',
         desc: 'Уменьшить -y[value](год), -m[value](месяц), -d[value](день)',
         handler: (argv) => {
-            const date = switchCalcDate(argv,'subtract')
-            console.log(date)
+            const date = new CalcDate(argv, 'subtract')
+            console.log(date.parseDate())
         }
     })
     .demandCommand()
     .help()
     .argv
-
-function switchCurrentDate(argv) {
-    switch (true) {
-        // eslint-disable-next-line no-prototype-builtins
-        case argv.hasOwnProperty('year'):
-            return `Текущий год: ${moment().year()}`
-        // eslint-disable-next-line no-prototype-builtins
-        case argv.hasOwnProperty('month'):
-            return `Текущий месяц: ${moment().format("MMMM")}`
-        // eslint-disable-next-line no-prototype-builtins
-        case argv.hasOwnProperty('days'):
-            return `Дней в календарном месяце:  ${moment().daysInMonth()}`;
-
-        default:
-            return `Текущая дата и время в формате ISO : ${moment().toISOString()}`
-    }
-}
-function switchCalcDate(argv,command) {
-    switch (true) {
-        // eslint-disable-next-line no-prototype-builtins
-        case argv.hasOwnProperty('year') && typeof argv.year === 'number' :
-            return `Изменение года на ${argv.year}: ${moment().clone()[command]( argv.year, 'years').toISOString()}`
-        // eslint-disable-next-line no-prototype-builtins
-        case argv.hasOwnProperty('month') && typeof argv.month === 'number':
-            return `Изменение месяца на  ${argv.month}: ${moment().clone()[command]( argv.month, 'months').toISOString()}`
-        // eslint-disable-next-line no-prototype-builtins
-        case argv.hasOwnProperty('day') && typeof argv.day === 'number':
-            return `Изменение дня  на ${argv.day} : ${moment().clone()[command]( argv.day, 'days').toISOString()}`
-        default:
-            return `Неверно указаны параметры (example: add -y5)`
-    }
-}
